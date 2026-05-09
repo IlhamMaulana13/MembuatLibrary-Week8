fe33ae3c533b4ac576144e81585803e3cb889160
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-05-09 20:01:18 +0700

    Menambah isi main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:mybutton_lib/mybutton_lib.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
