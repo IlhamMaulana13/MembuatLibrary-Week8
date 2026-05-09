1d68e4e66aa85de84a8756937330691faa69fd5f
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-05-09 16:33:13 +0700

    feat: initial release v1.0.0

--- lib/mybutton_lib.dart
+++ lib/mybutton_lib.dart
@@ -1,5 +1,20 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:mybutton_lib/src/custom_button.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CustomButton(
+          color: Colors.green,
+          label: 'Klik Saya',
+          borderRadius: BorderRadius.circular(8),
+          onPressed: () {
+            print('Tombol ditekan!');
+          },
+        ),
+      ),
+    );
+  }
 }
