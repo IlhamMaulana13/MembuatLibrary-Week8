a5194199180481ab4c393aff3bb519a59a84de1c
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-05-09 19:25:44 +0700

    menambahkan export folder src

--- lib/mybutton_lib.dart
+++ lib/mybutton_lib.dart
@@ -1,20 +1,2 @@
-import 'package:flutter/material.dart';
-import 'package:mybutton_lib/src/custom_button.dart';
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: CustomButton(
-          color: Colors.green,
-          label: 'Klik Saya',
-          borderRadius: BorderRadius.circular(8),
-          onPressed: () {
-            print('Tombol ditekan!');
-          },
-        ),
-      ),
-    );
-  }
-}
+export 'src/custom_button.dart';
+export 'src/custom_text.dart';
